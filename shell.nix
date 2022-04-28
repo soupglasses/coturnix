e45b39baff53eea62744ebd99bda6e04b4e809ab
Sofi <sofi+git@mailbox.org>
2022-04-28 17:50:35 +0200

    Switch to flakes

--- shell.nix
+++ shell.nix
@@ -1,23 +1,10 @@
-let
-
-  sources = import ./nix/sources.nix;
-
-  nixpkgs = sources."nixpkgs-unstable";
-
-  pkgs = import nixpkgs {};
-
-in pkgs.mkShell rec {
-
-  name = "home-manager-shell";
-
-  buildInputs = with pkgs; [
-    niv
-    (import sources.home-manager {inherit pkgs;}).home-manager
-  ];
-
-  shellHook = ''
-    export NIX_PATH="nixpkgs=${nixpkgs}:home-manager=${sources."home-manager"}"
-    export HOME_MANAGER_CONFIG="./home.nix"
-  '';
-
-}
+(import
+  (
+    let lock = builtins.fromJSON (builtins.readFile ./flake.lock); in
+    fetchTarball {
+      url = "https://github.com/edolstra/flake-compat/archive/${lock.nodes.flake-compat.locked.rev}.tar.gz";
+      sha256 = lock.nodes.flake-compat.locked.narHash;
+    }
+  )
+  { src = ./.; }
+).shellNix
