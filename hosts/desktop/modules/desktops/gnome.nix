647f3e90cc57634835f8523c014cac04ee26d1b0
Sofi <sofi+git@mailbox.org>
2022-09-09 19:42:38 +0200

    GNOME: Audio improvements

--- hosts/desktop/modules/desktops/gnome.nix
+++ hosts/desktop/modules/desktops/gnome.nix
@@ -16,8 +16,8 @@
   hardware.pulseaudio.enable = false;
   services.pipewire = {
     enable = true;
-    audio.enable = true;
-    jack.enable = true;
+    alsa.enable = true;
+    alsa.support32Bit = true;
     pulse.enable = true;
     wireplumber.enable = true;
   };
