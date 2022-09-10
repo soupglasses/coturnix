85110bce2961381acc070016d378fbd9ead93a24
Sofi <sofi+git@mailbox.org>
2022-09-10 20:36:14 +0200

    gnome: simplify audio and add gnome extentions

--- hosts/desktop/modules/desktops/gnome.nix
+++ hosts/desktop/modules/desktops/gnome.nix
@@ -1,24 +1,22 @@
 { config, pkgs, lib, ... }:
 {
-  services.xserver = {
+  services.xserver.enable = true;
+  services.xserver.desktopManager.gnome.enable = true;
+  services.xserver.displayManager.gdm = {
     enable = true;
-    displayManager.gdm = {
-      enable = true;
-      wayland = if (builtins.elem "nvidia" config.services.xserver.videoDrivers) then false else true;
-    };
-    desktopManager.gnome.enable = true;
+    wayland = if (builtins.elem "nvidia" config.services.xserver.videoDrivers) then false else true;
   };
 
-  #services.dbus.enable = true;
-  #services.dbus.packages = [ pkgs.gnome3.dconf ];
-  #services.udev.packages = [ pkgs.gnome3.gnome-settings-daemon ];
+  environment.systemPackages = with pkgs.gnomeExtensions; [
+    appindicator
+  ];
+
+  services.dbus.enable = true;
+  services.udev.packages = [ pkgs.gnome3.gnome-settings-daemon ];
 
   hardware.pulseaudio.enable = false;
-  services.pipewire = {
-    enable = true;
-    alsa.enable = true;
-    alsa.support32Bit = true;
-    pulse.enable = true;
-    wireplumber.enable = true;
-  };
+
+  services.pipewire.enable = true;
+  services.pipewire.pulse.enable = true;
+  services.pipewire.wireplumber.enable = true;
 }
