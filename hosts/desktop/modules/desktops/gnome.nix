342c6897158c8954eb5e0492403010582cdce500
Sofi <sofi+git@mailbox.org>
2022-09-22 17:31:01 +0200

    gnome: fix small mispelling

--- hosts/desktop/modules/desktops/gnome.nix
+++ hosts/desktop/modules/desktops/gnome.nix
@@ -19,4 +19,7 @@
   services.pipewire.enable = true;
   services.pipewire.pulse.enable = true;
   services.pipewire.wireplumber.enable = true;
+  services.pipewire.media-session.enable = false;
+  services.pipewire.alsa.support32Bit = true;
+  services.pipewire.alsa.enable = true;
 }
