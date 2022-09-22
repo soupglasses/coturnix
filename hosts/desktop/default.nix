817273e2ba253c2f889bdf33e318fe76e8b992e0
Sofi <sofi+git@mailbox.org>
2022-09-22 17:30:47 +0200

    desktop: change to zen kernel due to broken fsync

--- hosts/desktop/default.nix
+++ hosts/desktop/default.nix
@@ -12,7 +12,7 @@
     ./modules/keyboard/interception.nix
   ];
 
-  boot.kernelPackages = pkgs.linuxPackages_xanmod_latest;
+  boot.kernelPackages = pkgs.linuxPackages_zen;
 
   # Use the systemd-boot EFI boot loader.
   boot.loader.systemd-boot.enable = true;
@@ -60,7 +60,11 @@
       obs-studio-plugins.obs-nvfbc
       obs-studio-plugins.obs-vkcapture
     ];})
+    mullvad-vpn
+    deluge
   ];
+
+  services.mullvad-vpn.enable = true;
 
   system.stateVersion = "22.05";  # Do not touch.
 }
