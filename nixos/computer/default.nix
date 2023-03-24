7d509a2be46628c8a23ee07116225ab2b66cf9a5
Sofi <sofi+git@mailbox.org>
2023-03-24 17:44:42 +0100

    feat: fix wireguard

--- nixos/computer/default.nix
+++ nixos/computer/default.nix
@@ -62,4 +62,14 @@
   networking.useDHCP = false;
   networking.useNetworkd = false;
   networking.networkmanager.enable = true;
+
+   # wireguard trips rpfilter up
+   networking.firewall.extraCommands = ''
+     ip46tables -t mangle -I nixos-fw-rpfilter -p udp -m udp --sport 51820 -j RETURN
+     ip46tables -t mangle -I nixos-fw-rpfilter -p udp -m udp --dport 51820 -j RETURN
+   '';
+   networking.firewall.extraStopCommands = ''
+     ip46tables -t mangle -D nixos-fw-rpfilter -p udp -m udp --sport 51820 -j RETURN || true
+     ip46tables -t mangle -D nixos-fw-rpfilter -p udp -m udp --dport 51820 -j RETURN || true
+   '';
 }
