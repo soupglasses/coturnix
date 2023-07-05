0d66e9e97b3c6a1dff5fc9e550668dff15f3bac2
Sofi <sofi+git@mailbox.org>
2023-07-05 04:11:41 +0200

    mkSystem: add patches argument

--- nixos/lib/mkSystem.nix
+++ nixos/lib/mkSystem.nix
@@ -1,6 +1,16 @@
 {lib}: (
-  self: args:
-    self.inputs.nixpkgs.lib.nixosSystem {
+  self: args: let
+    nixpkgs =
+      if builtins.hasAttr "patches" args && args.patches != []
+      then
+        (import self.inputs.nixpkgs {inherit (args) system;}).applyPatches {
+          name = "nixpkgs-patched";
+          src = self.inputs.nixpkgs;
+          inherit (args) patches;
+        }
+      else self.inputs.nixpkgs;
+  in
+    import "${builtins.toString nixpkgs}/nixos/lib/eval-config.nix" {
       inherit (args) system;
       modules =
         args.modules
@@ -8,18 +18,21 @@
           {
             # TODO: Rework this into a module using allowUnfreePredicate.
             nixpkgs.config.allowUnfree = true;
+
+            # TODO: Don't just use all overlays like this.
             nixpkgs.overlays = lib.attrValues self.overlays;
+
             # Add hashes and dates from our flake to the NixOS version, easily see the status
             # of a machine with `nixos-version`.
             system.nixos.versionSuffix =
               lib.mkForce ".${
-                lib.substring 0 8 (self.lastModifiedDate or self.lastModified)
+                lib.substring 0 8 (self.lastModifiedDate or self.lastModified or "19700101")
               }.${
                 self.shortRev or "dirty"
               }";
             # Modified repos have no formal revision id. Drop setting revisions if the repo is modified.
             # See: https://github.com/NixOS/nix/pull/5385
-            system.configurationRevision = lib.mkIf (self ? rev) self.rev;
+            system.nixos.revision = lib.mkIf (self ? rev) self.rev;
           }
         ];
     }
