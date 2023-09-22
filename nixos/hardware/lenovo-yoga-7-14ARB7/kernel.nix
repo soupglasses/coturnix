68d818e3fa51aae9cc6b6eab069afa5dfb6fcb09
Sofi <sofi+git@mailbox.org>
2023-09-22 16:15:42 +0200

    hardware: simplify kernel for yoga 7 14arb7

--- nixos/hardware/lenovo-yoga-7-14ARB7/kernel.nix
+++ nixos/hardware/lenovo-yoga-7-14ARB7/kernel.nix
@@ -1,9 +1,12 @@
-{config, pkgs, lib, ...}: {
-  boot.kernelPackages = lib.mkForce pkgs.linuxPackages_latest;
-  boot.kernelParams = ["mem_sleep_default=deep" "amd_pstate=active"];
-  boot.extraModulePackages = with config.boot.kernelPackages; [zenpower];
-  boot.kernelModules = ["kvm-amd" "amd-pstate" "zenpower"];
-  boot.blacklistedKernelModules = ["acpi_cpufreq" "k10temp"]; # Disable ACPI cpufreq (in favor of p-state), and k10temp for zenpower
+{
+  pkgs,
+  ...
+}: {
+  boot.kernelPackages = pkgs.linuxPackages_latest;
+  #boot.kernelParams = ["mem_sleep_default=deep" "amd_pstate=active"];
+  #boot.extraModulePackages = with config.boot.kernelPackages; [zenpower];
+  #boot.kernelModules = ["kvm-amd" "amd-pstate" "zenpower"];
+  #boot.blacklistedKernelModules = ["acpi_cpufreq" "k10temp"]; # Disable ACPI cpufreq (in favor of p-state), and k10temp for zenpower
 
   hardware.opengl.extraPackages = with pkgs; [
     vaapiVdpau
