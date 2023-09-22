c0240680d428ab8d4b09fff26e09a8a5f85dd6c7
Sofi <sofi+git@mailbox.org>
2023-09-22 16:11:49 +0200

    yoga: sync kernel-zen

--- nixos/hosts/yoga/default.nix
+++ nixos/hosts/yoga/default.nix
@@ -1,4 +1,4 @@
-{pkgs, ...}:
+{pkgs, lib, ...}:
 {
   imports = [
     ./hardware.nix
@@ -8,6 +8,28 @@
     ../desktop/modules/keyboard/interception.nix
     ../../mixins/smartcard.nix
   ];
+
+  # Gaming
+  nix.settings = {
+    substituters = ["https://ezkea.cachix.org"];
+    trusted-public-keys = ["ezkea.cachix.org-1:ioBmUbJTZIKsHmWWXPe1FSFbeVe+afhfgqgTSNd34eI="];
+  };
+  programs.anime-game-launcher.enable = true;
+
+  boot.kernelPackages = lib.mkForce pkgs.linuxPackages_zen;
+  boot.kernelParams = [
+    # mitigations=off only has minimal performance improvements on Intel, as the
+    # default mitigations under linux leave SMT enabled. You may check this with:
+    #   $ grep . /sys/devices/system/cpu/vulnerabilities/*
+    # From personal testing, I saw about a 1-3% penalty for keeping this on auto.
+    # https://linuxreviews.org/HOWTO_make_Linux_run_blazing_fast_(again)_on_Intel_CPUs#Performance_implications
+    "mitigations=auto"
+    # Remove artificial penalties for split locks, which is useful for games run
+    # through Proton.
+    # https://www.phoronix.com/news/Linux-Splitlock-Hurts-Gaming
+    "split_lock_detect=off"
+  ];
+
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
   networking.hostName = "yoga";
