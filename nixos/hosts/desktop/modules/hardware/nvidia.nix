902cb35879baab59b595f354af40b758fab5a8a6
Sofi <sofi+git@mailbox.org>
2023-09-27 17:46:43 +0200

    nvidia: update and fixing VAAPI

--- nixos/hosts/desktop/modules/hardware/nvidia.nix
+++ nixos/hosts/desktop/modules/hardware/nvidia.nix
@@ -3,36 +3,53 @@
   pkgs,
   ...
 }: {
-  # Steam has a soft 32-bit requirement for a lot of games.
+  services.xserver.videoDrivers = ["nvidia"];
+
+  # While not technically a requirement for nvidia, we typically want hardware acceleration
+  # enabled by default with nvidia drivers.
   hardware.opengl.enable = true;
+
+  # Steam has a soft 32-bit requirement for a lot of games.
   hardware.opengl.driSupport32Bit = true;
 
   # Initialize NVIDIA drivers in kernel space, which helps with performance
   # slightly.
   hardware.nvidia.modesetting.enable = true;
 
-  # Downgrade due to regression: https://github.com/NVIDIA/open-gpu-kernel-modules/issues/511
+  # Manually managed nvidia package. Update by fetching changes in:
+  # https://github.com/NixOS/nixpkgs/blob/master/pkgs/os-specific/linux/nvidia-x11/default.nix
   hardware.nvidia.package = config.boot.kernelPackages.nvidiaPackages.mkDriver {
-    version = "535.104.05";
-    sha256_64bit = "sha256-L51gnR2ncL7udXY2Y1xG5+2CU63oh7h8elSC4z/L7ck=";
-    sha256_aarch64 = "sha256-J4uEQQ5WK50rVTI2JysBBHLpmBEWQcQ0CihgEM6xuvk=";
-    openSha256 = "sha256-0ng4hyiUt0rHZkNveFTo+dSaqkMFO4UPXh85/js9Zbw=";
-    settingsSha256 = "sha256-pS9W5LMenX0Rrwmpg1cszmpAYPt0Mx+apVQmOmLWTog=";
-    persistencedSha256 = "sha256-uqT++w0gZRNbzyqbvP3GBqgb4g18r6VM3O8AMEfM7GU=";
+    version = "535.113.01";
+    sha256_64bit = "sha256-KOME2N/oG39en2BAS/OMYvyjVXjZdSLjxwoOjyMWdIE=";
+    sha256_aarch64 = "sha256-mw/p5ELGTNcM4P94soJIGqpLMBJHSPf+z9qsGnISuCk=";
+    openSha256 = "sha256-SePRFb5S2T0pOmkSGflYfJkJBjG3Dx/Z0MjwnWccfcI=";
+    settingsSha256 = "sha256-hiX5Nc4JhiYYt0jaRgQzfnmlEQikQjuO0kHnqGdDa04=";
+    persistencedSha256 = "sha256-V5Wu8a7EhwZarGsflAhEQDE9s9PjuQ3JNMU1nWvNNsQ=";
   };
 
-  services.xserver.videoDrivers = ["nvidia"];
-
-  # Stop hardware flickering
+  # Stop hardware flickering.
   hardware.nvidia.powerManagement.enable = true;
 
-  # Enable vaapi support
-  hardware.opengl.extraPackages = with pkgs; [vaapiVdpau];
-
-  #  hardware.opengl.extraPackages = [
-  #    (pkgs.runCommand "nvidia-icd" { } ''
-  #      mkdir -p $out/share/vulkan/icd.d
-  #      cp ${pkgs.linuxPackages.nvidia_x11}/share/vulkan/icd.d/nvidia_icd.x86_64.json $out/share/vulkan/icd.d/nvidia_icd.json
-  #    '')
-  #  ];
+  # Enable vaapi support.
+  hardware.nvidia.open = true;
+  hardware.opengl.extraPackages = with pkgs; [nvidia-vaapi-driver];
+  environment.variables = {
+    # Libva is surprisingly bad at auto-detecting drivers.
+    # Without this, libva thinks we have no driver support for video acceleration.
+    LIBVA_DRIVER_NAME = "nvidia";
+    # OpenGL is bad at auto-detecting drivers.
+    # Always prioritizes nouveau, regardless if it's blacklisted.
+    __GLX_VENDOR_LIBRARY_NAME = "nvidia";
+    # GBM is bad at auto-detecting drivers.
+    # Technically only reported as an issue under Wayland, but at this point I got
+    # trust issues with these library detection features.
+    GBM_BACKEND = "nvidia-drm";
+    # EXPERIMENTAL: Use direct bindings for nvidia-vaapi to support Nvidia 525+.
+    NVD_BACKEND = "direct";
+    # Firefox needs an extra hand to support va-api on nvidia.
+    # NOTE: This requires extra configuration, see:
+    # https://github.com/elFarto/nvidia-vaapi-driver#firefox
+    MOZ_X11_EGL = "1";
+    MOZ_DISABLE_RDD_SANDBOX = "1";
+  };
 }
