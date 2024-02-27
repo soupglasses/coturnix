c4f2404b54fc04a3441f6f2e8ce65578c0f7cccb
Sofi <sofi+git@mailbox.org>
2024-02-27 15:51:49 +0100

    feat: create genpatch script

--- packages/default.nix
+++ packages/default.nix
@@ -1,4 +1,5 @@
 {pkgs}: {
+  genpatch = pkgs.callPackage ./genpatch {};
   nvim = pkgs.callPackage ./neovim {};
   ps3iso-utils = pkgs.callPackage ./ps3iso-utils {};
   tas2563-fw = pkgs.callPackage ./tas2563-fw {};
---
+++ packages/genpatch/default.nix
@@ -0,0 +1,37 @@
+{
+  writeShellScriptBin,
+  curl,
+  jq,
+  nix,
+}:
+writeShellScriptBin "genpatch" ''
+  set -euo pipefail
+  # Weakness: The API returns the commit for the current upstream you are merging
+  # to. If the patch gets merge conflicts, this will also break. Haven't found an
+  # easy way to get the "true" parent commit for the PR.
+
+  if [ $# -eq 0 ]; then
+    echo "No PR number supplied"
+    exit 1
+  fi
+
+  repo="NixOS/nixpkgs"
+  pr_number=$1
+
+  response=$(${curl}/bin/curl -s "https://api.github.com/repos/$repo/pulls/$pr_number")
+
+  latest_commit=$(echo $response | ${jq}/bin/jq -r '.head.sha')
+  base_commit=$(echo $response | ${jq}/bin/jq -r '.base.sha')
+
+  patch_url="https://github.com/NixOS/nixpkgs/compare/$base_commit...$latest_commit.patch"
+  sha256_sum=$(${nix}/bin/nix-prefetch-url --quiet $patch_url)
+
+  pr_title=$(echo $response | ${jq}/bin/jq -r '.title')
+  cat << EOF
+  # $pr_title - https://nixpk.gs/pr-tracker.html?pr=$pr_number
+  (builtins.fetchurl {
+    url = "$patch_url";
+    sha256 = "$sha256_sum";
+  })
+  EOF
+''
