fd4f308041f9e785e01717525cc37796bb78bc7c
Sofi <sofi+git@mailbox.org>
2024-03-26 10:53:38 +0100

    hardare(14arb7): disable speaker hack

--- hardware/lenovo-yoga-7-14ARB7/default.nix
+++ hardware/lenovo-yoga-7-14ARB7/default.nix
@@ -18,24 +18,24 @@
   hardware.cpu.amd.updateMicrocode = true;
 
   # Speakers
-  hardware.i2c.enable = true;
-  hardware.i2c.group = "wheel";
-  environment.systemPackages = with pkgs; [
-    i2c-tools
-  ];
-  systemd.services.yoga-bass-speaker-fix = {
-    after = ["systemd-suspend.service" "systemd-hibernate.service"];
-    requiredBy = ["systemd-suspend.service" "systemd-hibernate.service"];
-    wantedBy = ["multi-user.target"];
-    description = "Triggers the yoga7 bass-speaker toggle with i2c on boot and resume.";
-    serviceConfig = {
-      Type = "oneshot";
-      User = "root";
-      ExecStart = pkgs.writeShellScript "yoga-bass-speaker-fix" ''
-        ${pkgs.i2c-tools}/bin/i2cset -y 3 0x48 0x2 0 && echo "Successfully applied speaker fix!"
-      '';
-    };
-  };
+  #hardware.i2c.enable = true;
+  #hardware.i2c.group = "wheel";
+  #environment.systemPackages = with pkgs; [
+  #  i2c-tools
+  #];
+  #systemd.services.yoga-bass-speaker-fix = {
+  #  after = ["systemd-suspend.service" "systemd-hibernate.service"];
+  #  requiredBy = ["systemd-suspend.service" "systemd-hibernate.service"];
+  #  wantedBy = ["multi-user.target"];
+  #  description = "Triggers the yoga7 bass-speaker toggle with i2c on boot and resume.";
+  #  serviceConfig = {
+  #    Type = "oneshot";
+  #    User = "root";
+  #    ExecStart = pkgs.writeShellScript "yoga-bass-speaker-fix" ''
+  #      ${pkgs.i2c-tools}/bin/i2cset -y 3 0x48 0x2 0 && echo "Successfully applied speaker fix!"
+  #    '';
+  #  };
+  #};
 
   # Automatic screen orientation
   hardware.sensor.iio.enable = true;
