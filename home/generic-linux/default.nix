b0746f27e72c338a399e16aa11f55e29cf606ec2
Sofi <sofi+git@mailbox.org>
2024-09-24 12:51:52 +0200

    fix: generic-linux

--- home/generic-linux/default.nix
+++ home/generic-linux/default.nix
@@ -1,5 +1,4 @@
-{ lib, pkgs, ... }:
-{
+{config, ...}: {
   imports = [
     ./env.nix
     ./git.nix
@@ -7,6 +6,16 @@
 
   # Allow nix to deal with system paths outside of nix when managed by home-manager.
   targets.genericLinux.enable = true;
+
+  xdg.enable = true;
+  xdg.mime.enable = true;
+  xdg.systemDirs.config = ["/etc/xdg"];
+  xdg.systemDirs.data = [
+    "/usr/local/share"
+    "/usr/share"
+    "/var/lib/flatpak/exports/share"
+    "${config.home.homeDirectory}/.local/share/flatpak/exports/share"
+  ];
 
   home.sessionVariablesExtra = ''
     # WORKAROUND: https://github.com/nix-community/home-manager/issues/1439
--- home/generic-linux/git.nix
+++ home/generic-linux/git.nix
@@ -1,9 +1,14 @@
-{config, lib, ...}:
 {
+  config,
+  lib,
+  pkgs,
+  ...
+}: {
   config = lib.mkIf (config.programs.git.enable) {
     home.sessionVariablesExtra = ''
       # WORKAROUND: https://github.com/NixOS/nixpkgs/issues/160527
       export GIT_SSH="/usr/bin/ssh"
+      export SSL_CERT_FILE="${pkgs.cacert}/etc/ssl/certs/ca-bundle.crt"
     '';
   };
 }
